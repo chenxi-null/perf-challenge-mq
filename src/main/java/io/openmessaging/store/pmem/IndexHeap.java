899e010acdd11fa434d8b3f2c91bdf00cc48381d
chenxi20 <chenxi20@corp.netease.com>
2021-11-01 23:11:20 +0800

    feat: indexHeap block partition - pass the part of read

--- src/main/java/io/openmessaging/store/pmem/IndexHeap.java
+++ src/main/java/io/openmessaging/store/pmem/IndexHeap.java
@@ -46,6 +46,11 @@ public class IndexHeap {
     public void start() {
     }
 
+    MemoryBlock findFirstBlock(String topic, int queueId) throws IOException {
+        Heap heap = getHeap(topic, queueId);
+        return heap.memoryBlockFromHandle(heap.getRoot());
+    }
+
     MemoryBlock findLatestBlock(String topic, int queueId) throws IOException {
         String key = buildKey(topic, queueId);
         MemoryBlock block = latestBlocks.get(key);
@@ -60,14 +65,9 @@ public class IndexHeap {
 
     private MemoryBlock createMemoryBlock(String topic, int queueId) throws IOException {
         log.debug("creating memory block");
-        String dir = config.getPmemDir() + "/" + topic;
-        FileUtil.createDirIfNotExists(dir);
-        String filename = dir + "/" + queueId;
 
-        long heapSize = config.getPmemIndexHeapSize();
         long blockSize = config.getPmemIndexMemoryBlockSize();
-        Heap heap = Heap.exists(filename) ? Heap.openHeap(filename) : Heap.createHeap(filename, heapSize);
-        heaps.put(buildKey(topic, queueId), heap);
+        Heap heap = createHeap(topic, queueId);
 
         MemoryBlock memoryBlock = heap.allocateMemoryBlock(blockSize, true);
         heap.setRoot(memoryBlock.handle());
@@ -75,6 +75,24 @@ public class IndexHeap {
         initBlockWrotePosition(memoryBlock);
         log.info("created memory block");
         return memoryBlock;
+    }
+
+    private Heap getHeap(String topic, int queueId) throws IOException {
+        Heap heap = heaps.get(buildKey(topic, queueId));
+        if (heap != null) {
+            return heap;
+        }
+        Heap h = createHeap(topic, queueId);
+        heaps.put(buildKey(topic, queueId), h);
+        return h;
+    }
+
+    private Heap createHeap(String topic, int queueId) throws IOException {
+        String dir = config.getPmemDir() + "/" + topic;
+        FileUtil.createDirIfNotExists(dir);
+        String filename = dir + "/" + queueId;
+        return Heap.exists(filename) ?
+                Heap.openHeap(filename) : Heap.createHeap(filename, config.getPmemIndexHeapSize());
     }
 
     public void write(String topic, int queueId, long queueOffset, long msgBlockHandle) throws IOException {
@@ -99,10 +117,10 @@ public class IndexHeap {
         }
     }
 
-    private void appendIndexItemToBlock(long queueOffset, long msgBlockHandle, MemoryBlock block, long currBlockWrotePosition) {
-        block.setLong(currBlockWrotePosition, queueOffset);
-        block.setLong(currBlockWrotePosition + 8, msgBlockHandle);
-        block.setLong(0, currBlockWrotePosition + 16);
+    private void appendIndexItemToBlock(long queueOffset, long msgBlockHandle, MemoryBlock block, long blockWrotePosition) {
+        block.setLong(blockWrotePosition, queueOffset);
+        block.setLong(blockWrotePosition + 8, msgBlockHandle);
+        block.setLong(0, blockWrotePosition + 16);
         block.flush();
     }
 
@@ -114,10 +132,6 @@ public class IndexHeap {
         int wrotePosition = 8 + 8;
         memoryBlock.setLong(0, wrotePosition);
         return wrotePosition;
-    }
-
-    private Heap getHeap(String topic, int queueId) {
-        return heaps.get(buildKey(topic, queueId));
     }
 
     // only for testing
@@ -173,16 +187,21 @@ public class IndexHeap {
     // load in memory table
     public void load(TopicQueueTable topicQueueTable, String topic, int queueId) throws IOException {
         log.info("loading topicQueueTable, ({}, {})", topic, queueId);
-        // TODO: find first block
-        MemoryBlock block = findLatestBlock(topic, queueId);
+        MemoryBlock block = findFirstBlock(topic, queueId);
+        while (true) {
+            long blockWrotePosition = block.getLong(0);
+            for (long pos = 8 + 8; pos < blockWrotePosition; pos += 16) {
+                long queueOffset = block.getLong(pos);
+                long msgBlockHandle = block.getLong(pos + 8);
+                updateMemTable(topicQueueTable, topic, queueId, queueOffset, msgBlockHandle);
+            }
 
-        long currBlockWrotePosition = block.getLong(0);
-        for (long pos = 8 + 8; pos < currBlockWrotePosition; pos += 16) {
-            long queueOffset = block.getLong(pos);
-            long msgBlockHandle = block.getLong(pos + 8);
-            updateMemTable(topicQueueTable, topic, queueId, queueOffset, msgBlockHandle);
+            long nextBlockHandle = block.getLong(8);
+            if (nextBlockHandle <= 0) {
+                break;
+            }
+            block = getHeap(topic, queueId).memoryBlockFromHandle(nextBlockHandle);
         }
-        // TODO: load next block
     }
 
     void updateMemTable(TopicQueueTable topicQueueTable, String topic, int queueId, long queueOffset, long msgBlockHandle) {
